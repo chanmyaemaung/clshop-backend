3e086f083b1c96b1bb3ced518d2f796e3cf91aa0
Chen Lay <maungmaungyoker.mmy@gmail.com>
2024-06-16 14:32:00 +0700

    :label: Creating a data source and generating the first user migration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './domain/users/users.module';
 
 @Module({
   imports: [UsersModule, CommonModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
