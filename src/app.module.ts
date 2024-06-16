ca74cce16175a3fab9af0c47951bb609d66f9c5c
Chen Lay <maungmaungyoker.mmy@gmail.com>
2024-06-16 19:15:03 +0700

    :label: Creating payment entity and handling 121 relation.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { DatabaseModule } from './database/database.module';
+import { OrdersModule } from './domain/orders/orders.module';
+import { PaymentsModule } from './domain/payments/payments.module';
 import { UsersModule } from './domain/users/users.module';
 import { EnvModule } from './env/env.module';
-import { OrdersModule } from './domain/orders/orders.module';
 
 @Module({
-  imports: [UsersModule, CommonModule, DatabaseModule, EnvModule, OrdersModule],
+  imports: [
+    UsersModule,
+    CommonModule,
+    DatabaseModule,
+    EnvModule,
+    OrdersModule,
+    PaymentsModule,
+  ],
   controllers: [],
   providers: [],
 })
